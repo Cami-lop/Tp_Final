0e56b15e4798d665d9e57acd542293d161baba2a
Camila <camilalipez04@gmail.com>
2023-11-15 19:34:03 -0300

    Tp final parte 2

---
+++ Tp_Final.R
@@ -0,0 +1,72 @@
+#Tp Final Camila Lopez 
+#Abro el archivo ncdf 
+getwd()
+setwd("C:/Users/camil/OneDrive/Escritorio/Cami_Labo/Tp_Final/")
+require(ncdf4)  #llamo a la libreria que voy a necesitar 
+#abro el archivo
+archivo<-nc_open("C:/Users/camil/OneDrive/Escritorio/Cami_Labo/Tp_Final/daily_data_buenos_aires_province_1993-2023.nc")
+archivo
+# 4 dimen - variable lwe_precipitation_rate
+pp<-ncvar_get(archivo,varid="precip") #array de dime 9 lon/8 lat / 9892 dias 
+pp[which(pp == -9999)]<-NA # veo el datos faltante en la informacion del archivo y lo reemplazo con NA
+#1- tiempo cada dia desde 1970-01-01 00:00:00 0:00
+tiempo<-ncvar_get(archivo,varid="time")
+#2-longitud 
+lon<-ncvar_get(archivo,varid="longitude")
+#3-latitud
+lat<-ncvar_get(archivo,varid="latitude")
+
+#Lista con las 10 ciudades 
+ciudades<-list()
+datos_ciudades<-read.table("Ciudades.txt") #lee los datos del txt
+
+for (i in  1:10){
+  ciudad<-list("Nombre"= datos_ciudades[i,1], #preguntar si me conviene sacarlo
+                 "Latitud"=as.numeric(datos_ciudades[i,2]), #preguntar lo de hacerlo numerico
+                 "Longitud"=(360+(as.numeric(datos_ciudades[i,3])))
+  )
+ ciudades[[i]]<-ciudad 
+}
+
+names(ciudades)<-list("Bella Vista","Ezeiza","La Plata","Pergamino","San Nicolas","Bahia Blanca","Ayacucho","Monte Chingolo","Magdalena","Pinamar")
+#Calculos los puntos cercanos a cada ciudad
+puntos_cercanos <- list()
+lon_cerca <- c()
+lat_cerca <- c()
+for (i in 1:10) {
+  pos_cercana <- which.min(abs(lon - ciudades[[i]][[3]]))
+  pas_cercana <- which.min(abs(lat - ciudades[[i]][[2]]))
+  
+  lon_cerca[i] <- lon[pos_cercana]
+  lat_cerca[i] <- lat[pas_cercana]
+  
+  puntos_cercanos[[i]] <- list("Ciudad" = datos_ciudades[i, 1],
+                               "Latitud" = lat_cerca[i],
+                               "Longitud" = lon_cerca[i])
+}
+names(puntos_cercanos)<-list("Bella Vista","Ezeiza","La Plata","Pergamino","San Nicolas","Bahia Blanca","Ayacucho","Monte Chingolo","Magdalena","Pinamar")
+#lo convierto en un dataframe 
+puntos_df<-data.frame()
+for (i in 1:10) {
+  fila<-data.frame("Nombre"=c(puntos_cercanos[[i]][[1]]),
+                        "Longitudes"=c(puntos_cercanos[[i]][[3]]),
+                        "Latitudes"=c(puntos_cercanos[[i]][[2]]))
+  puntos_df<-rbind(puntos_df,fila)
+}
+#guardo el archivo 
+write.table(puntos_df, "dataciudades_pr.txt", sep = "\t", row.names = FALSE)
+###############################################################################
+require(lubridate) #llamo a las librerias que voy a necesitar 
+
+tiempos_legibles<- as.Date(tiempo,origin="1970-01-01 00:00:00 0:00 ")
+head(tiempos_legibles) #desde el 10/01/96 // se repite
+tail(tiempos_legibles) #hasta el 31/07/23 #son 26 años y 7 meses
+
+
+dias<-as.Date("1996/10/01"):as.Date("2023/07/31")
+datas<-array(data=pp,dim=c(9,8,366,27))
+
+media<-apply(datas,c(1,2,3),mean) #creo que es asi
+desvio<-apply(datas,c(1,2,3),sd)
+
+
